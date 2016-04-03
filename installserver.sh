9f677e83c35e9f01c34e838f31320b3adbb2db87
Igor1711 <Igor1711>
2016-04-03 16:07:53 +0300

    new commit

---
+++ installserver.sh
@@ -0,0 +1,7 @@
+apt-get update -y
+apt-get install -y python sudo mc w3m nginx
+apt-get update -y
+sudo apt-get install-y python-pip python-dev build-essential 
+apt-get update -y
+pip install django
+apt-get install -y gunicorn
